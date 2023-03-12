4f68a5a6a110679842458f0336568ae1a2a7c084
ixmael <hola@irm.mx>
2023-03-12 10:52:27 -0600

    add podspec

--- Sources/Fingerprint/FingerprintService.swift
+++ Sources/Fingerprint/FingerprintService.swift
@@ -24,7 +24,7 @@ public struct FingerprintService: FingerprintServiceProtocol {
                 metadata.setTag(environment, forKey: "environment")
             }
 
-            let fingerprintproDeviceID = try await fingerprintproService.getVisitorId(metadata)
+            _ = try await fingerprintproService.getVisitorId(metadata)
         } catch {
             print("error", error as Any)
         }
